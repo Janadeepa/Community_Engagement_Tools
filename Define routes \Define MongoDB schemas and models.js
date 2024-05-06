b4410ae247d0dd3b1ea3014dcf574f6ccc435477
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2024-05-06 22:30:06 +0530

    Create Define routes \Define MongoDB schemas and models.js

---
+++ Define routes \Define MongoDB schemas and models.js
@@ -0,0 +1,15 @@
+//3 . Define MongoDB schemas and models:
+  //Define Mongoose schemas for questions and answers in your app.js file.
+
+//4 . Define routes:
+  //Create routes for handling CRUD operations on questions and answers. You can use Express Router for this purpose. 
+//Here's an example:
+
+const express = require('express');
+const router = express.Router();
+
+// Define routes for handling CRUD operations on questions
+
+// Define routes for handling CRUD operations on answers
+
+module.exports = router;
